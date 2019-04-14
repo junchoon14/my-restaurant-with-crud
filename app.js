67a07fb1f6dd9ffc2c4859194dac5c96abb61125
Alvis Hsu <junchoon14@gmail.com>
2019-04-14 09:17:33 +0800

    Add mongoose connection and create restaurant model

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+mongoose.connect('mongodb://localhost/my_restaurant', { useNewUrlParser: true })
+const db = mongoose.connection
+
+// connenet error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// connect success
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Restaurant = require('./models/restaurant')
 
 // setting static files
 app.use(express.static('public'))
---
+++ models/restaurant.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  name_en: {
+    type: String,
+  },
+  category: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+  },
+  google_map: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+})
